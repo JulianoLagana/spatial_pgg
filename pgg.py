d31405e022e4b9682efeab5db0310c8eb8af210a
Juliano Lagana <juliano@chalmers.se>
2020-11-28 19:56:29 +0100

    Make compute_pff_payoffs return payoffs, not resulting players' money

--- pgg.py
+++ pgg.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def compute_pgg_payoffs(players_money: np.ndarray, contributions: np.ndarray, mult_factor: float) -> float:
+def compute_pgg_payoffs(players_money: np.ndarray, contributions: np.ndarray, mult_factor: float) -> np.ndarray:
     assert len(players_money) == len(contributions), '`players_money` has to have the same number of elements as `contributions`'
     assert np.all(contributions <= players_money), f'At least one of the players is contributing more money than he has!' \
                                                    f'\nContributions: {contributions}' \
@@ -10,12 +10,15 @@ def compute_pgg_payoffs(players_money: np.ndarray, contributions: np.ndarray, mu
     # Compute dividend (same for all players)
     divvy = np.sum(contributions)/len(contributions)*mult_factor
 
-    # Return resulting money for all players
-    return players_money - contributions + divvy
+    # Return payoffs for each player
+    return divvy - contributions
 
 
-players_money = np.array([100, 100, 100])
-contributions = np.array([0, 100, 20])
+# Example usage
+if __name__ == '__main__':
+    players_money = np.array([100, 100, 100])
+    contributions = np.array([0, 100, 20])
 
-players_money = compute_pgg_payoffs(players_money, contributions, 1.5)
-print(players_money)
+    payoffs = compute_pgg_payoffs(players_money, contributions, 1.5)
+    players_money = players_money + payoffs
+    print(players_money)

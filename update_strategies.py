dfc02d7361ee1a38ab3c30a7dc2d7c3a9f89771d
Juliano Lagana <juliano@chalmers.se>
2020-11-28 20:14:09 +0100

    Creates docstring for soft_noisy_update_according_to_neighbor

--- update_strategies.py
+++ update_strategies.py
@@ -7,7 +7,20 @@ def soft_noisy_update_according_to_best_neighbor(player_money: float,
                                                  neighbors_strategy: np.array,
                                                  neighbors_payoff: np.array,
                                                  alpha: float,
-                                                 noise_intensity: float):
+                                                 noise_intensity: float) -> float:
+    """
+    Function used to update a player's strategy given knowledge about what other players played and their payoffs in the
+     previous round.
+    @param player_money: Maximum value this player can contribute in each round.
+    @param player_strategy: How much money this player contributed in the previous round.
+    @param player_payoff: Payoff received by this player from playing in the previous round.
+    @param neighbors_strategy: Strategies used by this player's neighbors in the previous round.
+    @param neighbors_payoff: Payoff received by each of this player's neighbors.
+    @param alpha: Decides how soft the update will be. 0 corresponds to no update, 1 to hard update.
+    @param noise_intensity: Intensity of the noise added to the resulting computed strategy.
+    @return: Updated strategy for this player.
+    """
     best_neighbor_strategy = neighbors_strategy[np.argmax(neighbors_payoff)]
     noise = np.random.normal(0, noise_intensity)
-    return np.clip(alpha*best_neighbor_strategy + (1-alpha)*player_strategy + noise, 0, player_money)
+    new_strategy = np.clip(alpha*best_neighbor_strategy + (1-alpha)*player_strategy + noise, 0, player_money)
+    return float(new_strategy)
